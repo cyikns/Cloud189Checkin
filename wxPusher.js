ad19d374f1ebc614f72599276646817a428a936d
cyikns <8066193@qq.com>
2024-03-07 09:27:05 +0800

    Update wxPusher.js
    更新AppToken

--- wxPusher.js
+++ wxPusher.js
@@ -1,4 +1,4 @@
 module.exports = {
-    appToken: process.env.WX_PUSHER_APP_TOKEN || 'AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr',
+    appToken: process.env.WX_PUSHER_APP_TOKEN || 'AT_g2XxOloVPY4ETzLQYtfrNpWwz1Mzx39Z',
     uid: process.env.WX_PUSHER_UID||''
   };
